f5ba6ecb9b84d346ce36c97ca627cb718f047b25
Trung DG <thanhtrung.it25@gmail.com>
2021-04-24 19:23:52 +0700

    CardButtons handler

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,15 +6,23 @@ import { ShoppingCart } from "@material-ui/icons";
 import { Badge } from "@material-ui/core";
 
 import useStyles from "./styles";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar({ totalItems }) {
   const classes = useStyles();
+  const location = useLocation();
 
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit">
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
             <img
               src={logo}
               alt="Commerce.js"
@@ -24,13 +32,20 @@ export default function Navbar({ totalItems }) {
             Commerce.js
           </Typography>
           <div className={classes.grow} />
-          <div className={classes.button}>
-            <IconButton aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
-          </div>
+          {location.pathname === "/" && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </>
